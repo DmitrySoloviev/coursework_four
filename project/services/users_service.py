a5029a011d5bd0c083c5afc6e9abadd2f9c02fdf
Дмитрий <DmitrySoloviev>
2022-10-24 23:56:30 +0300

    Change user_service

--- project/services/users_service.py
+++ project/services/users_service.py
@@ -1,11 +1,14 @@
-from project.dao.base import BaseDAO
+from project.dao.main import UsersDAO
 from project.exceptions import ItemNotFound
 from project.models import User
 from typing import Optional
 
+from project.tools.security import generate_tokens, approve_refresh_token, get_data_from_token
+from project.tools.security import generate_password_hash
+
 
 class UsersService:
-    def __init__(self, dao: BaseDAO) -> None:
+    def __init__(self, dao: UsersDAO) -> None:
         self.dao = dao
 
     def get_item(self, pk: int) -> User:
@@ -15,3 +18,34 @@ class UsersService:
 
     def get_all(self, page: Optional[int] = None) -> list[User]:
         return self.dao.get_all(page=page)
+
+    def create(self, email, password):
+        return self.dao.create(email, password)
+
+    def get_user_by_login(self, email):
+        return self.dao.get_user_by_login(email)
+
+    def check(self, email, password):
+        user = self.get_user_by_login(email)
+        return generate_tokens(email=email, password=password, password_hash=user.password)
+
+    def update_token(self, refresh_token):
+        return approve_refresh_token(refresh_token)
+
+    def get_user_by_token(self, refresh_token):
+        data = get_data_from_token(refresh_token)
+        if data:
+            return self.get_user_by_login(data.get('email'))
+
+    def update_user(self, data: dict, refresh_token):
+        user = self.get_user_by_token(refresh_token)
+        if user:
+            self.dao.update(email=user.email, data=data)
+            return self.get_user_by_token(refresh_token)
+
+    def update_password(self, data: dict, refresh_token):
+        user = self.get_user_by_token(refresh_token)
+        if user:
+            self.dao.update(email=user.email,
+                            data={"password": generate_password_hash(data.get('password_2'))})
+            return self.check(login=user.email, password=data.get('password_2'))
